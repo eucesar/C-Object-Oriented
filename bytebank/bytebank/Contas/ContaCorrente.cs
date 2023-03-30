fe6a89213dab4eb613c35aa67d9fbc58b5a70404
Cesar Iglesias <cesaribneto.job@gmail.com>
2023-03-29 21:16:22 -0300

    alura-curso-c+

---
+++ bytebank/bytebank/Contas/ContaCorrente.cs
@@ -0,0 +1,97 @@
+﻿using bytebank.Titular;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//eu crio uma class para interlegar outros .cs = td PUBLIC 
+//separar variavel
+namespace bytebank.Contas
+{
+    public class ContaCorrente
+    {
+        public static int TotalDeContasCriadas { get; private set; }
+
+        private int numero_agencia;
+        public int Numero_agencia
+        {
+            get { return this.numero_agencia; }
+            private set
+            {
+                if (value > 0)
+                {
+                    this.numero_agencia = value;
+                }
+            }
+        }
+
+
+        public string Conta { get; set; }
+        //eu privei so nesse codigo
+        private double saldo = 100;
+
+        //cliquei na lampada pq poem no altomatico = Ttilar.Cliente
+        public Cliente titular;
+
+        public void Depositar(double valor) //vou definir o valor dentro do 'Depositar' fora do class
+        {
+            saldo += valor;
+        }
+
+        //boll = condicionalA
+        public bool Sacar(double valor)
+        {
+            if (valor <= saldo) //tem q ser manor ou igual ao valor para poder sacar
+            {
+                saldo -= valor; //se tiver saldo - o valor do saque for menor ou igual = pq preciso de dinheiro para sacar, n pode ser maior q o valor - pq dai n tenho dinheiro
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor) //se o saldo que eu quer transferir for menor que o valor tt da conta
+            {
+                return false;
+            }
+            else
+            {
+                Sacar(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+        }
+
+        //SET = alterar
+        //GET = pegar
+        public void SetSaldo(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            else
+            {
+                this.saldo = valor;
+            }
+        }
+
+        public double GetSaldo()
+        {
+            return this.saldo;
+        }
+
+        public ContaCorrente(int numero_agencia, string numero_conta)
+        {
+            this.Numero_agencia = numero_agencia;
+            this.Conta = numero_conta;
+            TotalDeContasCriadas++;
+        }
+    }
+
+}
---
+++ bytebank/bytebank/Program.cs
@@ -0,0 +1,84 @@
+﻿using bytebank.Contas;
+using bytebank.Titular;
+
+/*
+Cliente cliente = new Cliente();
+cliente.nome = "André Silva";
+cliente.cpf = "123456789";
+cliente.profissao = "Analista";
+
+ContaCorrente conta = new ContaCorrente();
+conta.titular = cliente;
+conta.conta = "1010-X";
+conta.numero_agencia = 15;
+conta.saldo = 100;
+
+Console.WriteLine("Titular = " + conta.titular.nome);
+Console.WriteLine("Cpf = " + conta.titular.cpf);
+Console.WriteLine("Profissão = " + conta.titular.profissao);
+Console.WriteLine("Nº Conta = " + conta.conta);
+Console.WriteLine("Saldo = " + conta.saldo);
+Console.WriteLine("Nº Agência = " + conta.numero_agencia);
+
+ContaCorrente conta2 = new ContaCorrente();
+//junto os dois new 
+conta2.titular = new Cliente();
+conta2.titular.nome = "José Souza";
+conta2.titular.profissao = "Tester";
+conta2.titular.cpf = "987654321";
+conta2.conta = "9999-X";
+conta2.numero_agencia = 18;
+conta2.saldo = 500;
+
+Console.WriteLine(conta2.titular.nome);
+*/
+
+/*
+//crio um novo OBJETO = que chama variaveis ou qq coisa da class / repartição
+ContaCorrente contaDoAndre = new ContaCorrente();
+contaDoAndre.titular = "André Silva"; //uso o PONTO depois do nome da varivel que eu acabei de definir para chamar a var q está na class
+contaDoAndre.numero_agencia = 15;
+contaDoAndre.conta = "1010-X";
+contaDoAndre.saldo = 100;
+
+Console.WriteLine("Saldo da conta do André = " + contaDoAndre.saldo);
+*/
+
+/*
+contaDoAndre.Depositar(100);
+
+Console.WriteLine("Saldo da conta do André pós-depósito = " + contaDoAndre.saldo);
+
+if (contaDoAndre.Sacar(300) == true) //IGUAL
+{
+    Console.WriteLine("Saldo da conta do André pós-saque = " + contaDoAndre.saldo);
+}
+else
+{
+    Console.WriteLine("Saldo insuficiente para saque.");
+}
+*/
+
+/*
+ContaCorrente contaDaMaria = new ContaCorrente();
+contaDaMaria.titular = "Maria Souza";
+contaDaMaria.numero_agencia = 17;
+contaDaMaria.conta = "1010-5";
+contaDaMaria.saldo = 350;
+
+Console.WriteLine("Saldo da conta da Maria = " + contaDaMaria.saldo);
+
+contaDoAndre.Transferir(50, contaDaMaria);
+
+Console.WriteLine("Saldo do André = " + contaDoAndre.saldo);
+Console.WriteLine("Saldo da Maria = " + contaDaMaria.saldo);
+*/
+
+ContaCorrente conta5 = new ContaCorrente(283, "1234-X");
+Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+ContaCorrente conta6 = new ContaCorrente(284, "9874-Z");
+Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+ContaCorrente conta7 = new ContaCorrente(285, "1111-Z");
+Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
